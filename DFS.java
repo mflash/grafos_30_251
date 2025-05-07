79defdcdecefd903aa740f5eaf441174f5d580f2
Marcelo Cohen <mflashbr@gmail.com>
2025-05-07 20:42:34 -0300

    Implementação completa do DFS

--- DFS.java
+++ DFS.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class DFS
 {
+    private Graph g;
     private Set<String> marked;
     private Map<String, String> edgeTo;
     private String inicial;
@@ -12,6 +15,7 @@ public class DFS
     public DFS(Graph g, String inicial)
     {
         this.inicial = inicial;
+        this.g = g;
         marked = new HashSet<>();
         edgeTo = new HashMap<>();
         for(String v: g.getVerts()) {
@@ -22,11 +26,62 @@ public class DFS
 
     private void dfs(Graph g, String inicial)
     {
-        System.out.println("visitando: "+inicial);        
+        System.out.println("entrando: "+inicial);
+        marked.add(inicial); // marca como visitado
+        for(String w: g.getAdj(inicial)) {
+            if(!marked.contains(w)) {
+                edgeTo.put(w, inicial);
+                dfs(g, w);           
+            }     
+        }
+        System.out.println("saindo  c: "+inicial);
+    }
+
+    public boolean hasPathTo(String v) {
+        return marked.contains(v);
+    }
+
+    public List<String> pathTo(String v)
+    {
+        if(!marked.contains(v))
+            return new ArrayList<>();
+        List<String> path = new ArrayList<>();
+        String atual = edgeTo.get(v);
+        while(atual != null) {
+            path.add(0, atual);
+            atual = edgeTo.get(atual);            
+        }
+        return path;
+    }
+
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        for(String v: g.vertices) {            
+            sb.append(v+": ");
+            if(!marked.contains(v)) {
+                sb.append(" sem caminho");
+            }
+            else
+            {                
+                sb.append(pathTo(v));
+                
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
         Graph g = new Graph("exemplos/tinyG.txt");
         DFS dfs = new DFS(g, "0");        
+        System.out.println(dfs);
+
+        if(dfs.hasPathTo("7")) {
+            System.out.println("Tem caminho para 7");
+        }
+        else {
+            System.out.println("Não consigo chegar no 7!");
+        }
     }
 }
