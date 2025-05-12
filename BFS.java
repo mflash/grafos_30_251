a9a2051d2c8a39b1ff638e2c869e51b943ac54da
Marcelo Cohen <mflashbr@gmail.com>
2025-05-12 20:42:45 -0300

    BFS completo + grafo "médio"

--- BFS.java
+++ BFS.java
@@ -1,72 +1,74 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class BFS
-{
+public class BFS {
     private Graph g;
     private Set<String> marked;
     private Map<String, String> edgeTo;
     private Map<String, Integer> distTo;
     private String inicial;
 
-    public BFS(Graph g, String inicial)
-    {
+    public BFS(Graph g, String inicial) {
         this.inicial = inicial;
         this.g = g;
         marked = new HashSet<>();
         edgeTo = new HashMap<>();
         distTo = new HashMap<>();
-        for(String v: g.getVerts()) {
+        for (String v : g.getVerts()) {
             edgeTo.put(v, null);
         }
         bfs(g, inicial);
     }
 
-    private void dfs(Graph g, String inicial)
-    {
-        System.out.println("entrando: "+inicial);
+    private void bfs(Graph g, String inicial) {
+        List<String> fila = new LinkedList<>();
         marked.add(inicial); // marca como visitado
-        for(String w: g.getAdj(inicial)) {
-            if(!marked.contains(w)) {
-                // implementar bfs
-            }     
+        distTo.put(inicial, 0);
+        fila.add(inicial);
+        while (!fila.isEmpty()) {
+            String v = fila.removeFirst();
+            for (String w : g.getAdj(v)) {
+                if (!marked.contains(w)) {
+                    fila.add(w);
+                    marked.add(w);
+                    edgeTo.put(w, v);
+                    distTo.put(w, distTo.get(v) + 1);
+                }
+            }
         }
-        System.out.println("saindo: "+inicial);
     }
 
     public boolean hasPathTo(String v) {
         return marked.contains(v);
     }
 
-    public List<String> pathTo(String v)
-    {
-        if(!marked.contains(v))
+    public List<String> pathTo(String v) {
+        if (!marked.contains(v))
             return new ArrayList<>();
         List<String> path = new ArrayList<>();
+        path.add(v);
         String atual = edgeTo.get(v);
-        while(atual != null) {
+        while (atual != null) {
             path.add(0, atual);
-            atual = edgeTo.get(atual);            
+            atual = edgeTo.get(atual);
         }
         return path;
     }
 
-    public String toString()
-    {
+    public String toString() {
         StringBuilder sb = new StringBuilder();
-        for(String v: g.vertices) {            
-            sb.append(v+": ");
-            if(!marked.contains(v)) {
+        for (String v : g.vertices) {
+            sb.append(v + ": ");
+            if (!marked.contains(v)) {
                 sb.append(" sem caminho");
-            }
-            else
-            {                
-                sb.append(pathTo(v));
-                
+            } else {
+                sb.append(pathTo(v) + " (" + distTo.get(v) + ")");
+
             }
             sb.append("\n");
         }
@@ -75,14 +77,19 @@ public class BFS
 
     public static void main(String[] args) {
         Graph g = new Graph("exemplos/tinyG.txt");
-        BFS bfs = new BFS(g, "0");        
+        // Graph g = new Graph("exemplos/mediumG.txt");
+        long start = System.nanoTime();
+        BFS bfs = new BFS(g, "0");
+        long end = System.nanoTime();
         System.out.println(bfs);
+        long delta = end - start;
+        System.out.println("Tempo: " + delta + " ns");
 
-        if(bfs.hasPathTo("7")) {
-            System.out.println("Tem caminho para 7");
-        }
-        else {
-            System.out.println("Não consigo chegar no 7!");
+        String destino = "3";
+        if (bfs.hasPathTo(destino)) {
+            System.out.println("Tem caminho para " + destino);
+        } else {
+            System.out.println("Não consigo chegar no " + destino + "!");
         }
     }
 }
--- DFS.java
+++ DFS.java
@@ -5,67 +5,61 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class DFS
-{
+public class DFS {
     private Graph g;
     private Set<String> marked;
     private Map<String, String> edgeTo;
     private String inicial;
 
-    public DFS(Graph g, String inicial)
-    {
+    public DFS(Graph g, String inicial) {
         this.inicial = inicial;
         this.g = g;
         marked = new HashSet<>();
         edgeTo = new HashMap<>();
-        for(String v: g.getVerts()) {
+        for (String v : g.getVerts()) {
             edgeTo.put(v, null);
         }
         dfs(g, inicial);
     }
 
-    private void dfs(Graph g, String inicial)
-    {
-        System.out.println("entrando: "+inicial);
+    private void dfs(Graph g, String inicial) {
+        System.out.println("entrando: " + inicial);
         marked.add(inicial); // marca como visitado
-        for(String w: g.getAdj(inicial)) {
-            if(!marked.contains(w)) {
+        for (String w : g.getAdj(inicial)) {
+            if (!marked.contains(w)) {
                 edgeTo.put(w, inicial);
-                dfs(g, w);           
-            }     
+                dfs(g, w);
+            }
         }
-        System.out.println("saindo  c: "+inicial);
+        System.out.println("saindo  c: " + inicial);
     }
 
     public boolean hasPathTo(String v) {
         return marked.contains(v);
     }
 
-    public List<String> pathTo(String v)
-    {
-        if(!marked.contains(v))
+    public List<String> pathTo(String v) {
+        if (!marked.contains(v))
             return new ArrayList<>();
         List<String> path = new ArrayList<>();
+        path.add(v);
         String atual = edgeTo.get(v);
-        while(atual != null) {
+        while (atual != null) {
             path.add(0, atual);
-            atual = edgeTo.get(atual);            
+            atual = edgeTo.get(atual);
         }
         return path;
     }
 
-    public String toString()
-    {
+    public String toString() {
         StringBuilder sb = new StringBuilder();
-        for(String v: g.vertices) {            
-            sb.append(v+": ");
-            if(!marked.contains(v)) {
+        for (String v : g.vertices) {
+            sb.append(v + ": ");
+            if (!marked.contains(v)) {
                 sb.append(" sem caminho");
-            }
-            else
-            {                
+            } else {
                 sb.append(pathTo(v));
-                
+
             }
             sb.append("\n");
         }
@@ -74,13 +68,17 @@ public class DFS
 
     public static void main(String[] args) {
         Graph g = new Graph("exemplos/tinyG.txt");
-        DFS dfs = new DFS(g, "0");        
+        // Graph g = new Graph("exemplos/mediumG.txt");
+        long start = System.nanoTime();
+        DFS dfs = new DFS(g, "0");
+        long end = System.nanoTime();
         System.out.println(dfs);
+        long delta = end - start;
+        System.out.println("Tempo: " + delta + " ns");
 
-        if(dfs.hasPathTo("7")) {
+        if (dfs.hasPathTo("7")) {
             System.out.println("Tem caminho para 7");
-        }
-        else {
+        } else {
             System.out.println("Não consigo chegar no 7!");
         }
     }
