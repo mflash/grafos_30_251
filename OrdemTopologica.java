883063b58a89dd3b49327d7e037ca282313395cd
Marcelo Cohen <mflashbr@gmail.com>
2025-05-19 20:28:35 -0300

    Ordem topológica (completa)

--- OrdemTopologica.java
+++ OrdemTopologica.java
@@ -1,85 +1,50 @@
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public class OrdemTopologica {
     private Graph g;
     private Set<String> marked;
-    private Map<String, String> edgeTo;
-    private String inicial;
+    private List<String> ordemTopo;    
 
-    public OrdemTopologica(Graph g, String inicial) {
-        this.inicial = inicial;
+    public OrdemTopologica(Graph g) {    
         this.g = g;
         marked = new HashSet<>();
-        edgeTo = new HashMap<>();
-        for (String v : g.getVerts()) {
-            edgeTo.put(v, null);
+        ordemTopo = new LinkedList<>();
+        for(String v: g.getVerts()) {
+            if(!marked.contains(v))
+                dfs(g,v);
         }
-        dfs(g, inicial);
     }
 
     private void dfs(Graph g, String inicial) {
-        System.out.println("entrando: " + inicial);
+        // System.out.println("entrando: " + inicial);
         marked.add(inicial); // marca como visitado
         for (String w : g.getAdj(inicial)) {
             if (!marked.contains(w)) {
-                edgeTo.put(w, inicial);
                 dfs(g, w);
             }
         }
-        System.out.println("saindo  c: " + inicial);
+        // Inclui o vértice no INÍCIO da lista
+        // de ordem topológica (pós-ordem REVERSA)
+        ordemTopo.add(0, inicial);
+        // System.out.println("saindo  c: " + inicial);
     }
 
-    public boolean hasPathTo(String v) {
-        return marked.contains(v);
-    }
-
-    public List<String> pathTo(String v) {
-        if (!marked.contains(v))
-            return new ArrayList<>();
-        List<String> path = new ArrayList<>();
-        path.add(v);
-        String atual = edgeTo.get(v);
-        while (atual != null) {
-            path.add(0, atual);
-            atual = edgeTo.get(atual);
-        }
-        return path;
-    }
-
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (String v : g.vertices) {
-            sb.append(v + ": ");
-            if (!marked.contains(v)) {
-                sb.append(" sem caminho");
-            } else {
-                sb.append(pathTo(v));
-
-            }
-            sb.append("\n");
-        }
-        return sb.toString();
+    public Iterable<String> getOrdemTopologica()
+    {
+        return ordemTopo;
     }
 
     public static void main(String[] args) {
-        Graph g = new Graph("exemplos/tinyG.txt");
+        Digraph g = new Digraph("exemplos/tinyG.txt");
         // Graph g = new Graph("exemplos/mediumG.txt");
-        long start = System.nanoTime();
-        DFS dfs = new DFS(g, "0");
-        long end = System.nanoTime();
-        System.out.println(dfs);
-        long delta = end - start;
-        System.out.println("Tempo: " + delta + " ns");
-
-        if (dfs.hasPathTo("7")) {
-            System.out.println("Tem caminho para 7");
-        } else {
-            System.out.println("Não consigo chegar no 7!");
-        }
+        OrdemTopologica ot = new OrdemTopologica(g);
+        System.out.println("Ordem topológica:");
+        for(String v: ot.getOrdemTopologica()) {
+            System.out.println("-> "+v);
+        }        
+        System.out.println(g.toDot());
     }
 }
